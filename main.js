fd91f460167de3e8b3e7380e91df5b45fb5d274a
Faishal Rachman <faishalr97@gmail.com>
2020-04-21 20:32:16 +0700

    fix(env) : adding dotenv

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const router = require('./routes/router');
+require('dotenv').config()
 const token = process.env.token || "-";
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
